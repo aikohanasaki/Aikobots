e454aac3d4397cfcf6dabe7e2e11ad9cf3c686f1
aikohanasaki <future.skyline@gmail.com>
2025-09-26 01:04:54 -0600

    turn off talky debug

--- public/scripts/RossAscends-mods.js
+++ public/scripts/RossAscends-mods.js
@@ -466,7 +466,7 @@ function restoreUserInput() {
 function saveUserInput() {
     const userInput = String($('#send_textarea').val());
     localStorage.setItem(getUserInputKey(), userInput);
-    console.debug('User Input -- ', userInput);
+    // console.debug('User Input -- ', userInput);
 }
 const saveUserInputDebounced = debounce(saveUserInput);
 
