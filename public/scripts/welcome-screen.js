27193b13566f1b3a9b72fb35b7f6949167632fca
aikohanasaki <future.skyline@gmail.com>
2026-06-15 17:45:00 -0600

    removed extra assistant avatar

--- public/scripts/welcome-screen.js
+++ public/scripts/welcome-screen.js
@@ -10,7 +10,6 @@ import {
     getCharacters,
     getCurrentChatId,
     getRequestHeaders,
-    getSystemMessageByType,
     getThumbnailUrl,
     is_send_press,
     neutralCharacterName,
@@ -191,8 +190,7 @@ export async function openWelcomeScreen({ force = false, expand = false } = {}) 
 
     await sendWelcomePanel(recentChats, expand);
     await unshallowPermanentAssistant();
-    sendAssistantMessage();
-    sendWelcomePrompt();
+    await sendAssistantMessage();
 }
 
 /**
@@ -209,21 +207,23 @@ async function unshallowPermanentAssistant() {
     await unshallowCharacter(String(characterId));
 }
 
-function sendAssistantMessage() {
+async function sendAssistantMessage() {
     const currentAssistantAvatar = getPermanentAssistantAvatar();
     const character = characters.find(x => x.avatar === currentAssistantAvatar);
     const name = character ? character.name : neutralCharacterName;
     const avatar = character ? getThumbnailUrl('avatar', character.avatar) : system_avatar;
+    const welcomePrompt = await renderTemplateAsync('welcomePrompt');
 
     const message = {
         name: name,
         force_avatar: avatar,
-        mes: t`# 🎉 __**Welcome to Aikobots!**__ 🎉` + '\n\n' + t`We're glad you're here. If you ever need help, head to the #help-911 channel in our Discord server.`  + '\n\n' + t`**Now that you're here:**` + '\n' + t`3️⃣ Connect to an API (click :fa-plug: in the top menu bar)` + '\n' + t`4️⃣ Create a persona for yourself (click :fa-face-smile: in the top menu bar)` + '\n' + t`5️⃣ Choose a character to play with! (click :fa-address-card: in the top menu bar)`  + '\n\n' + '\n\n' + t`**If you can't decide who to play with:**` + '\n' + '\n' + t`💬 Talk to Okia if you want recommendations or to do some analysis!` + '\n' +  t`📇 Check out the [Character Roll Call](https://www.aikobots.com/rollcall.html) for bots and blurbs` +'\n\n' + '\n\n' + t`**For commands, be sure to take a look at:** 📜 [List of all Aikobots Commands](https://www.aikobots.com/commands.html)` + '\n***\n' + t`💡 **PS:** Set any character as your welcome page assistant from their "More..." menu.`,
+        mes: t`# 🎉 __**Welcome to Aikobots!**__ 🎉` + '\n\n' + t`We're glad you're here. If you ever need help, head to the #help-911 channel in our Discord server.`  + '\n\n' + t`**Now that you're here:**` + '\n' + t`3️⃣ Connect to an API (click :fa-plug: in the top menu bar)` + '\n' + t`4️⃣ Create a persona for yourself (click :fa-face-smile: in the top menu bar)` + '\n' + t`5️⃣ Choose a character to play with! (click :fa-address-card: in the top menu bar)`  + '\n\n' + '\n\n' + t`**If you can't decide who to play with:**` + '\n' + '\n' + t`💬 Talk to Okia if you want recommendations or to do some analysis!` + '\n' +  t`📇 Check out the [Character Roll Call](https://www.aikobots.com/rollcall.html) for bots and blurbs` + '\n\n' + '\n\n' + t`**For commands, be sure to take a look at:** 📜 [List of all Aikobots Commands](https://www.aikobots.com/commands.html)` + '\n***\n' + t`💡 **PS:** Set any character as your welcome page assistant from their "More..." menu.` + '\n\n' + welcomePrompt,
         is_system: false,
         is_user: false,
         send_date: getMessageTimeStamp(),
         extra: {
             type: system_message_types.ASSISTANT_MESSAGE,
+            uses_system_ui: true,
         },
     };
 
@@ -231,7 +231,7 @@ function sendAssistantMessage() {
     addOneMessage(message, { scroll: false });
     postProcessAssistantIcons();
 }
- 
+
 function postProcessAssistantIcons() {
     try {
         const chatElement = document.getElementById('chat');
@@ -242,7 +242,7 @@ function postProcessAssistantIcons() {
         console.warn('Icon token post-process failed:', e);
     }
 }
- 
+
 function replaceIconTokensInElement(root) {
     const TOKEN_REGEX = /:fa-([a-z0-9-]+):/gi;
     // Collect matching text nodes first to avoid walker invalidation during replacement
@@ -256,7 +256,7 @@ function replaceIconTokensInElement(root) {
         }
         TOKEN_REGEX.lastIndex = 0; // reset due to global regex
     }
- 
+
     for (const textNode of textNodes) {
         const text = textNode.nodeValue || '';
         const frag = document.createDocumentFragment();
@@ -277,18 +277,12 @@ function replaceIconTokensInElement(root) {
         }
     }
 }
- 
+
 function createFaIcon(name) {
     const i = document.createElement('i');
     i.classList.add('fa-solid', `fa-${name}`);
     i.setAttribute('aria-hidden', 'true');
     return i;
-}
- 
-function sendWelcomePrompt() {
-    const message = getSystemMessageByType(system_message_types.WELCOME_PROMPT);
-    chat.push(message);
-    addOneMessage(message, { scroll: false });
 }
 
 /**
